3559bf30d7a547b88fdf76a01fd52c9224d223e9
valpo <m.waack@gmx.de>
2013-04-11 17:10:40 +0200

    add docs

--- fritzbox/__init__.py
+++ fritzbox/__init__.py
@@ -1,3 +1,8 @@
+'''
+  A package to access several FritzBox functions from the command line. 
+  Tested with a 7390 and Fritz!OS 05.50
+'''
+
 from fritzglobals import readConfig
 from fritzlogin import login
 from fritzautohome import getDevices, getConsumption
