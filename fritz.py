542589ab375118935ed34ff095c5929cec31e9bf
valpo <m.waack@gmx.de>
2016-05-07 18:22:14 +0200

    fritz!os 6.51 breaks everything, these are the first fixes to keep my munin stuff running

--- fritz.py
+++ fritz.py
@@ -1,4 +1,5 @@
 import fritzbox as fb
+from time import sleep
 import os
 
 def updateRrrd(powerconsumption):
@@ -13,21 +14,32 @@ def main():
   fb.readConfig("/home/mathias/.fritzboxpyrc")
   fb.fritzglobals.DEBUG = 0 # set this to 1 to see verbose output (may include passwords!)
   fb.login() # initialize session
+  fb.fritzglobals.DEBUG = 1
   devices = fb.getDevices() # get a list of all devices known by the box
-  device = devices[0] # I have only one device
-  devid = device[0] # id is always the first value
-  powerconsumption = fb.getConsumption(devid)
-  print "current consumption:",powerconsumption
-  state = fb.getPowerState(devid)
-  print "current state:",state
-  fb.powerOff(devid)
-  state = fb.getPowerState(devid)
-  print "current state:",state
-  fb.powerOn(devid)
-  state = fb.getPowerState(devid)
-  print "current state:",state
+  for devid in devices:
+    print "checking device",devid
+    powerconsumption = fb.getConsumption(devid)
+    print "current consumption:",powerconsumption
+    temp = fb.getTemperature(devid)
+    print "current temperatur:",temp
+    state = fb.getPowerState(devid)
+    print "current state:",state
+    print "sleeping a bit..."
+    sleep(21*60)
+    state = fb.getPowerState(devid)
+    print "current state:",state
+    powerconsumption = fb.getConsumption(devid)
+    print "current consumption:",powerconsumption
+    continue
+    # be carefull here....
+    fb.powerOff(devid)
+    state = fb.getPowerState(devid)
+    print "current state:",state
+    fb.powerOn(devid)
+    state = fb.getPowerState(devid)
+    print "current state:",state
 
-  updateRrrd(powerconsumption)
+  #updateRrrd(powerconsumption)
   
 
 if __name__ == "__main__":
--- fritzbox/fritzautohome.py
+++ fritzbox/fritzautohome.py
@@ -10,74 +10,49 @@ import fritzlogin
 def getDevices():
   ''' returns a list of tuples (deviceid, connectstate, switchstate) '''
   baseurl = fg.url
-  url = baseurl + "/net/home_auto_query.lua"
-  post_data = urllib.urlencode({'sid' : fg.SID, 'command' : 'AllOutletStates', 'xhr' : 0 })
+  url = baseurl + "/webservices/homeautoswitch.lua"
+  post_data = urllib.urlencode({'sid' : fg.SID, 'switchcmd' : 'getswitchlist' })
   req = urllib2.urlopen(url + '?' + post_data)
   data = req.read()
   if fg.DEBUG:
     print "data from autohome:"
     print req.info()
     print data
-  data = json.loads(data)
-  count = int(data["Outlet_count"])
-  res = []
-  for i in range(1,count+1):
-    device = (int(data["DeviceID_%d" % i]), int(data["DeviceConnectState_%d" % i]), int(data["DeviceSwitchState_%d" % i]))
-    res.append(device)
-  return res
+  data = data.strip().split(",")
+  return data
 
 def getTemperature(deviceid):
   ''' get the temperature of this device in celsius '''
-  baseurl = fg.url + "/net/home_auto_query.lua"
-  post_data = urllib.urlencode({'sid' : fg.SID, 'command' : 'AllOutletStates', 'xhr' : 0 })
+  baseurl = fg.url + "/webservices/homeautoswitch.lua"
+  post_data = urllib.urlencode({'sid' : fg.SID, 'switchcmd' : 'gettemperature', 'ain' : deviceid })
   req = urllib2.urlopen(baseurl + '?' + post_data)
   data = req.read()
   if fg.DEBUG:
     print "data from temperature:"
     print req.info()
     print data
-  data = json.loads(data)
+  data = float(data.strip())/10.0
   if fg.DEBUG:
     print data
-  for i in xrange(len(data)): # we cannot have more devices then entries in this list
-    try:
-      if data["DeviceID_%d" % i] == str(deviceid):
-        return float(data["DeviceTemp_%d" % i])/10.0
-    except KeyError: pass
-  if fg.DEBUG: print "no temperature for device",deviceid,"found"
-  return 0
+  return data
 
-def getConsumption(deviceid, timerange = "10"):
-  ''' get the average power consumption of this device for the given time range. range may be 10, 24h, month or year'''
-  tranges = ("10","24h","month","year")
-  if timerange not in tranges: raise FritzError("unknown timerange, possible values are: %s" % str(tranges))
-  baseurl = fg.url + "/net/home_auto_query.lua"
-  post_data = urllib.urlencode({'sid' : fg.SID, 'command' : 'EnergyStats_%s' % timerange, 'id' : deviceid, 'xhr' : 0 })
+def getConsumption(deviceid):
+  ''' get the current power consumption of this device '''
+  baseurl = fg.url + "/webservices/homeautoswitch.lua"
+  post_data = urllib.urlencode({'sid' : fg.SID, 'switchcmd' : 'getswitchpower', 'ain' : deviceid })
   req = urllib2.urlopen(baseurl + '?' + post_data)
   data = req.read()
   if fg.DEBUG:
     print "data from power consumption:"
     print req.info()
     print data
-  data = json.loads(data)
-  average = float(data["EnStats_average_value"])/100.0
-  if fg.DEBUG: print "average:", average
-  return average
+  data = float(data.strip())/1000.0
+  if fg.DEBUG: print "average:", data
+  return data
 
 def getLastConsumption(deviceid):
   ''' get the last known (latest) power consumption of this device '''
-  baseurl = fg.url + "/net/home_auto_query.lua"
-  post_data = urllib.urlencode({'sid' : fg.SID, 'command' : 'EnergyStats_10', 'id' : deviceid, 'xhr' : 0 })
-  req = urllib2.urlopen(baseurl + '?' + post_data)
-  data = req.read()
-  if fg.DEBUG:
-    print "data from power consumption:"
-    print req.info()
-    print data
-  data = json.loads(data)
-  latest = float(data["EnStats_watt_value_1"])/100.0
-  if fg.DEBUG: print "latest:", latest
-  return latest
+  return getConsumption(deviceid)
 
 def getPowerState(deviceid):
   ''' returns either 0 (power off) or 1 (power on) '''
